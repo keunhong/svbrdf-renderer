f38bb2ad283584abf7a1e86f60b7fec9a242d38b
Konstantinos Rematas <krematas@gmail.com>
2016-06-18 15:07:30 -0700

    check where the program is run

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import vispy
 from vispy import app
 from svbrdf import renderer, SVBRDF
+import socket
 
+if socket.gethostname() == 'Konstantinoss-MacBook-Pro.local':
+    app.use_app('glfw')
+    datapath = '/Users/kostas/Documents/Projects/wood_door'
+else:
+    app.use_app('pyglet')
+    datapath = '/Volumes/Seagate/Research/data/svbrdf/twoshot_data_results/leather_antique'
 
-app.use_app('pyglet')
-
-svbrdf = SVBRDF('/Volumes/Seagate/Research/data/svbrdf/twoshot_data_results/leather_antique')
+svbrdf = SVBRDF(datapath)
 canvas = renderer.Canvas(svbrdf, size=(800, 600))
 
 app.run()
